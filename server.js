8d02d4bbc5a110e70130f6a96f9a3ac9596f9aa5
lswitlearning <lswitlearning@gmail.com>
2023-12-17 13:45:28 +1100

    Add some comment

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
-const express= require("express");
+const express = require("express");
 const res = require("express/lib/response");
-const app= express();
+const app = express();
 
-app.get('/addTwoNumbers/:firstNumber/:secondNumber', function(req,res,next){
-    var firstNumber = parseInt(req.params.firstNumber) 
-    var secondNumber = parseInt(req.params.secondNumber)
-    var result = firstNumber + secondNumber || null
-    if(result == null) {
-      res.json({result: result, statusCode: 400}).status(400)
-    }
-    else { res.json({result: result, statusCode: 200}).status(200) } 
-  })
+app.get('/addTwoNumbers/:firstNumber/:secondNumber', function (req, res, next) {
+  var firstNumber = parseInt(req.params.firstNumber)
+  var secondNumber = parseInt(req.params.secondNumber)
+  var result = firstNumber + secondNumber || null   // Calculate the sum of the two numbers or set result to null if conversion fails
+  if (result == null) {
+    res.json({ result: result, statusCode: 400 }).status(400)
+  }
+  else {
+    res.json({ result: result, statusCode: 200 }).status(200)
+  }
+})
 
 
-const port=3000;
-app.listen(port,()=> {
-    console.log("hello i'm listening to port "+port);
+const port = 3000;
+app.listen(port, () => {
+  console.log("hello i'm listening to port " + port);
 })
--- test/test.js
+++ test/test.js
@@ -1,64 +1,77 @@
-var expect  = require("chai").expect;
+var expect = require("chai").expect;
 var request = require("request");
 
-describe("Add Two Numbers", function() {
-    var url = "http://localhost:3000/addTwoNumbers/3/5";
-    it("returns status 200 to check if api works", function(done) {
-        request(url, function(error, response, body) {
-            expect(response.statusCode).to.equal(200);
-            done()
-          });
+// Test suite for the "Add Two Numbers" API endpoint
+describe("Add Two Numbers", function () {
+  var url = "http://localhost:3000/addTwoNumbers/12/13";  // Define the URL for the API endpoint
+  it("returns status 200 to check if api works", function (done) {  // Test case: Check if the API returns status 200
+    request(url, function (error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      done()
     });
-    it("returns statusCode key in body to check if api give right result should be 200", function(done) {
-        request(url, function(error, response, body) {
-            body = JSON.parse(body)
-            expect(body.statusCode).to.equal(200);
-            done()
-          });
-    });
-    it("returns the result as number", function(done) {
-        request(url, function(error, response, body) {
-            body = JSON.parse(body)
-            expect(body.result).to.be.a('number');
-            done()
-          });
-    });
-    it("returns the result equal to 8", function(done) {
-      request(url, function(error, response, body) {
-          body = JSON.parse(body)
-          expect(body.result).to.equal(8);
-          done()
-        });
-  });
-  it("returns the result not equal to 15", function(done) {
-    request(url, function(error, response, body) {
-        body = JSON.parse(body);
-        expect(body.result).to.not.equal(15);
-        done()
-      });
-});
   });
 
-  describe("Add Two strings", function() {
-    var url = "http://localhost:3000/addTwoNumbers/a/b";
-    it("should return status 200", function(done) {
-        request(url, function(error, response, body) {
-            expect(response.statusCode).to.equal(200);
-            done()
-          });
-    });
-    it("returns statusCode key in body to check if api gives right result should be 400", function(done) {
-        request(url, function(error, response, body) {
-            body = JSON.parse(body)
-            expect(body.statusCode).to.equal(400);
-            done()
-          });
-    });
-    it("returns the result as null", function(done) {
-        request(url, function(error, response, body) {
-            body = JSON.parse(body)
-            expect(body.result).to.be.a('null');
-            done()
-          });
+  // Test case: Check if the API returns statusCode key in the body and it is equal to 200
+  it("returns statusCode key in body to check if api give right result should be 200", function (done) {
+    request(url, function (error, response, body) {
+      body = JSON.parse(body)       // Parse the response body as JSON
+      expect(body.statusCode).to.equal(200);
+      done()
     });
   });
+
+  // Test case: Check if the API returns the result as a number
+  it("returns the result as number", function (done) {
+    request(url, function (error, response, body) {
+      body = JSON.parse(body)
+      expect(body.result).to.be.a('number');
+      done()
+    });
+  });
+
+  // Test case: Check if the API returns the result equal to (?)
+  it("returns the result equal to 25", function (done) {
+    request(url, function (error, response, body) {
+      body = JSON.parse(body)
+      expect(body.result).to.equal(25);
+      done()
+    });
+  });
+
+  // Test case: Check if the API returns the result not equal to 15
+  it("returns the result not equal to 15", function (done) {
+    request(url, function (error, response, body) {
+      body = JSON.parse(body);
+      expect(body.result).to.not.equal(15);
+      done()
+    });
+  });
+});
+
+// Test suite for the "Add Two Strings" API endpoint
+describe("Add Two strings", function () {
+  var url = "http://localhost:3000/addTwoNumbers/a/b";
+  it("should return status 200", function (done) {   // Test case: Check if the API returns status 200
+    request(url, function (error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      done()
+    });
+  });
+  // Test case: Check if the API returns statusCode key in the body and it is equal to 400
+  it("returns statusCode key in body to check if api gives right result should be 400", function (done) {
+    request(url, function (error, response, body) {
+      body = JSON.parse(body)
+      expect(body.statusCode).to.equal(400);
+      done()
+    });
+  });
+
+  // Test case: Check if the API returns the result as null
+  it("returns the result as null", function (done) {
+    request(url, function (error, response, body) {
+      body = JSON.parse(body)
+      expect(body.result).to.be.a('null');
+      done()
+    });
+  });
+});
